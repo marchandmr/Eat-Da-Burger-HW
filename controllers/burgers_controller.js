58491e08b3caeddde6e86105e13ab84807539bf6
Matthew Marchand <marchandmr@icloud.com>
2021-01-21 13:19:54 -0600

    application is functional but needs css

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -11,5 +11,27 @@ router.get("/", function (req, res) {
         res.render("index", hbsObject);
     });
 });
+router.put("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition);
+    burger.updateOne({
+        devoured: true
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+router.post("/api/burgers", function (req, res) {
+    burger.createOne([
+        "burger_name"
+    ], [
+        req.body.name
+    ], function (result) {
+        res.json({ id: result.insertId });
+    });
+});
 
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -7,6 +7,16 @@ var burger =
             cb(res);
         });
     },
+    updateOne: function (objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function (res) {
+            cb(res);
+        });
+    },
+    createOne: function (cols, vals, cb) {
+        orm.createOne("burgers", cols, vals, function (res) {
+            cb(res);
+        });
+    }
 }
 
 module.exports = burger;
